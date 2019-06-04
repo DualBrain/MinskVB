82623a6956720e43501b57834b0d00a7e25265ee
Cory Smith <corysmith@addressof.com>
2019-06-04 16:13:19 -0500

    Add test project (currently targetting .NET Core 2.1 as it appears that xUnit isn't available for 3.0 yet).

---
+++ BASIC/Basic.Tests/UnitTest1.vb
@@ -0,0 +1,19 @@
+Option Explicit On
+Option Strict On
+Option Infer On
+
+Imports Xunit
+
+Namespace Basic.Tests
+
+  Public Class UnitTest1
+
+    <Fact>
+    Sub TestSub()
+
+    End Sub
+
+  End Class
+
+End Namespace
+
