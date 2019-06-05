b9f8677fdf813c9b90394a69b512c7b6f844ad82
Cory Smith <corysmith@addressof.com>
2019-06-05 10:35:42 -0500

    Replace is checks with select case in Evaluator.

--- BASIC/Basic/CodeAnalysis/Evaluator.vb
+++ BASIC/Basic/CodeAnalysis/Evaluator.vb
@@ -22,57 +22,65 @@ Namespace Global.Basic.CodeAnalysis
 
     Private Function EvaluateExpression(node As BoundExpression) As Object
 
-      If TypeOf node Is BoundLiteralExpression Then
-        Return DirectCast(node, BoundLiteralExpression).Value
-      End If
+      Select Case node.Kind
+        Case BoundNodeKind.LiteralExpression : Return Me.EvaluateLiteralExpression(node)
+        Case BoundNodeKind.VariableExpression : Return Me.EvaluateVariableExpression(node)
+        Case BoundNodeKind.AssignmentExpression : Return Me.EvaluateAssignmentExpression(node)
+        Case BoundNodeKind.UnaryExpression : Return Me.EvaluateUnaryExpression(node)
+        Case BoundNodeKind.BinaryExpression : Return Me.EvaluateBinaryExpression(node)
+        Case Else
+          Throw New Exception($"Unexpected node {node.Kind}")
+      End Select
 
-      If TypeOf node Is BoundVariableExpression Then
-        Dim v = DirectCast(node, BoundVariableExpression)
-        Return Me.Variables(v.Variable)
-      End If
+    End Function
 
-      If TypeOf node Is BoundAssignmentExpression Then
-        Dim a = DirectCast(node, BoundAssignmentExpression)
-        Dim value = Me.EvaluateExpression(a.expression)
-        Me.Variables(a.Variable) = value
-        Return value
-      End If
+    Private Function EvaluateLiteralExpression(node As BoundExpression) As Object
+      Return DirectCast(node, BoundLiteralExpression).Value
+    End Function
 
-      If TypeOf node Is BoundUnaryExpression Then
-        Dim u = DirectCast(node, BoundUnaryExpression)
-        Dim operand = Me.EvaluateExpression(u.Operand)
-        Select Case u.Op.Kind
-          Case BoundUnaryOperatorKind.Identity
-            Return CInt(operand)
-          Case BoundUnaryOperatorKind.Negation
-            Return -CInt(operand)
-          Case BoundUnaryOperatorKind.LogicalNegation
-            Return Not CBool(operand)
-          Case Else
-            Throw New Exception($"Unexpected unary operator {u.Op}")
-        End Select
-      End If
+    Private Function EvaluateVariableExpression(node As BoundExpression) As Object
+      Dim v = DirectCast(node, BoundVariableExpression)
+      Return Me.Variables(v.Variable)
+    End Function
 
-      If TypeOf node Is BoundBinaryExpression Then
-        Dim b = DirectCast(node, BoundBinaryExpression)
-        Dim left = Me.EvaluateExpression(b.Left)
-        Dim right = Me.EvaluateExpression(b.Right)
-        Select Case b.Op.Kind
-          Case BoundBinaryOperatorKind.Addition : Return CInt(left) + CInt(right)
-          Case BoundBinaryOperatorKind.Subtraction : Return CInt(left) - CInt(right)
-          Case BoundBinaryOperatorKind.Multiplication : Return CInt(left) * CInt(right)
-          Case BoundBinaryOperatorKind.Division : Return CInt(left) \ CInt(right)
-          Case BoundBinaryOperatorKind.LogicalAnd : Return CBool(left) And CBool(right)
-          Case BoundBinaryOperatorKind.LogicalOr : Return CBool(left) Or CBool(right)
-          Case BoundBinaryOperatorKind.Equals : Return Equals(left, right)
-          Case BoundBinaryOperatorKind.NotEquals : Return Not Equals(left, right)
-          Case Else
-            Throw New Exception($"Unexpected binary operator {b.Op}")
-        End Select
-      End If
+    Private Function EvaluateAssignmentExpression(node As BoundExpression) As Object
+      Dim a = DirectCast(node, BoundAssignmentExpression)
+      Dim value = Me.EvaluateExpression(a.Expression)
+      Me.Variables(a.Variable) = value
+      Return value
+    End Function
 
-      Throw New Exception($"Unexpected node {node.Kind}")
+    Private Function EvaluateUnaryExpression(node As BoundExpression) As Object
+      Dim u = DirectCast(node, BoundUnaryExpression)
+      Dim operand = Me.EvaluateExpression(u.Operand)
+      Select Case u.Op.Kind
+        Case BoundUnaryOperatorKind.Identity
+          Return CInt(operand)
+        Case BoundUnaryOperatorKind.Negation
+          Return -CInt(operand)
+        Case BoundUnaryOperatorKind.LogicalNegation
+          Return Not CBool(operand)
+        Case Else
+          Throw New Exception($"Unexpected unary operator {u.Op}")
+      End Select
+    End Function
 
+    Private Function EvaluateBinaryExpression(node As BoundExpression) As Object
+      Dim b = DirectCast(node, BoundBinaryExpression)
+      Dim left = Me.EvaluateExpression(b.Left)
+      Dim right = Me.EvaluateExpression(b.Right)
+      Select Case b.Op.Kind
+        Case BoundBinaryOperatorKind.Addition : Return CInt(left) + CInt(right)
+        Case BoundBinaryOperatorKind.Subtraction : Return CInt(left) - CInt(right)
+        Case BoundBinaryOperatorKind.Multiplication : Return CInt(left) * CInt(right)
+        Case BoundBinaryOperatorKind.Division : Return CInt(left) \ CInt(right)
+        Case BoundBinaryOperatorKind.LogicalAnd : Return CBool(left) And CBool(right)
+        Case BoundBinaryOperatorKind.LogicalOr : Return CBool(left) Or CBool(right)
+        Case BoundBinaryOperatorKind.Equals : Return Equals(left, right)
+        Case BoundBinaryOperatorKind.NotEquals : Return Not Equals(left, right)
+        Case Else
+          Throw New Exception($"Unexpected binary operator {b.Op}")
+      End Select
     End Function
 
   End Class
