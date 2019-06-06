d76a3bd86b673f84dc6e62688bfe4e064f37809a
Cory Smith <corysmith@addressof.com>
2019-06-05 21:01:26 -0500

    Fixed null reference for cases where the "missing/expected" token was inserted.

--- BASIC/Basic/CodeAnalysis/Syntax/SyntaxToken.vb
+++ BASIC/Basic/CodeAnalysis/Syntax/SyntaxToken.vb
@@ -23,7 +23,7 @@ Namespace Global.Basic.CodeAnalysis.Syntax
 
     Public Overrides ReadOnly Property Span As TextSpan
       Get
-        Return New TextSpan(Me.Position, Me.Text.Length)
+        Return New TextSpan(Me.Position, If(Me.Text?.Length, 0))
       End Get
     End Property
 
